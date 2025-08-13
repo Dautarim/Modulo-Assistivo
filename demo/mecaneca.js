1ee289cee3dd0bb218d5c62c1e092e115b598d3e
Dautarim Osvaldo Carvalho Andrelino <75495689+Dautarim@users.noreply.github.com>
2025-08-13 00:25:15 +0000

    menu completo

--- demo/mecaneca.js
+++ demo/mecaneca.js
@@ -3,7 +3,7 @@
 
 const itemDoMenu = document.querySelectorAll("#menuItem")
 
-itemDoMenu.forEach((e)=>{e.addEventListener('click', ()=>{
+itemDoMenu.forEach((e)=>{e.addEventListener('mouseover', ()=>{
         itemDoMenu.forEach((i) =>  i.querySelector('#submenu')?.classList.remove('hide'));
         e.querySelector('#submenu').classList.toggle('hide') }) })
 
